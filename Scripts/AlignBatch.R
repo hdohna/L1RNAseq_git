e0cbce41070bf2b71d96d35c0f04b9e766f8df87
hdohna <heinrich.dohna@gmail.com>
2016-07-07 10:10:53 -0700

    Modified paths for SCG4
    Modified paths to fit SCG4 file structure. Also added options to run bwa
    as aligner instead of tophat.

--- Scripts/AlignBatch.R
+++ Scripts/AlignBatch.R
@@ -2,22 +2,26 @@
 # data, creates scripts to align them and sends off the scripts
 
 # Source start script
-source('/home/hzudohna/L1polymORF/Scripts/_Start_L1polymORF_bix2.R')
+source('/home/hzudohna/L1polymORFgit/Scripts/_Start_L1polymORF_scg4.R')
 
 # Specify path to batch folder and pattern for data folder
-BatchPath <- '/share/diskarray4/MEI/expression/batch2'
+BatchPath <- '/srv/gsfs0/projects/levinson/hzudohna/RNAseq/'
 DataPattern <- "sample"
 
 # Specify align command and options
-AlignCommand <- '/share/diskarray1/Updated_RNA_software/TopHat/tophat-2.0.9.Linux_x86_64/tophat2' 
-AlignOptions <- '--library-type fr-firststrand -o' 
+AlignCommand <- c('module load tophat', 'module load bowtie2', 'tophat2') 
+AlignOptions <- '--library-type fr-firststrand --max-multihits 1 -o' 
+# AlignCommand <- c('module load bwa', 'bwa mem') 
+# AlignOptions <- '-t6' 
+AlignTool <- strsplit(AlignCommand[1], 'module load ')[[1]][2]
 
 # Specify reference path
-RefPath <- '/home/hzudohna/L1polymORF/Data/L1Catalog200FlankSameStrand_Sat_May_07_15-15-31_2016'
-ScriptFile <- "/home/hzudohna/qsubScriptAlignRNA"
+RefPath   <- '/srv/gsfs0/projects/levinson/hzudohna/RefSeqData/L1Catalog200FlankSameStrand_Sat_May_07_15-15-31_2016'
+#RefPath   <- '/srv/gsfs0/projects/levinson/hzudohna/RefSeqData/L1Catalog200FlankSameStrand_Sat_May_07_15-15-31_2016.fas'
+ScriptDir <- "/home/hzudohna/"
 
 # Path to output directory
-OutputPath <- '/share/diskarray3/hzudohna/RNAseq/'
+OutputPath <- '/srv/gsfs0/projects/levinson/hzudohna/RNAseq/'
 
 # Get all paths to fastq files in batch folder
 DataDirs <- list.dirs(BatchPath, recursive = F)
@@ -25,19 +29,31 @@ DataDirs <- grep(DataPattern, DataDirs, value = T)
 
 # Loop through directories create a qsub file for alignment and submit it
 Dir <- DataDirs[1]
-for (Dir in DataDirs[-1]){
+for (Dir in DataDirs[8]){
   FastqFile1 <- list.files(Dir, pattern = '.trimmed.R1.fastq', full.names = T, recursive = T)
   FastqFile2 <- list.files(Dir, pattern = '.trimmed.R2.fastq', full.names = T, recursive = T)
   DirSplit  <- strsplit(Dir, "/")[[1]]
   LastDirPart <- DirSplit[length(DirSplit)]
   OutDir    <-  paste(OutputPath, LastDirPart, sep = "")
+#  OutDir    <-  paste(OutDir, AlignTool, sep = "/")
   if (!dir.exists(OutDir)) dir.create(OutDir)
-  if (length(list.files(OutDir)) == 0){
-    CommandLine <- paste(AlignCommand, AlignOptions, OutDir, RefPath, FastqFile1, FastqFile2)
+  ScriptFile <- paste(ScriptDir, "qsubAlign_", LastDirPart, AlignTool, sep = "")
+  if (!dir.exists(OutDir)) dir.create(OutDir)
+#  if (length(list.files(OutDir)) == 0){
+    CommandLine <- c(AlignCommand[1:2], paste(AlignCommand[3], AlignOptions, 
+                                            OutDir, RefPath, FastqFile1, FastqFile2))
+    # CommandLine <- c(AlignCommand[1], 
+    #                  paste(AlignCommand[2],RefPath, FastqFile1, FastqFile2,
+    #                      ">", paste(OutDir, "/", LastDirPart, "_alnBWA.sam", sep = "")))
     cat("Running script", ScriptFile, "\n")
     cat("Saving results to", OutDir, "\n")
-    CreateAndCallqsubScript(file = ScriptFile, qsubCommandLines = CommandLine, 
-                            scriptName = 'AlignRNAseq')
+    CreateAndCallqsubScript(file = ScriptFile, 
+       qsubHeaderLines = c('#! /bin/sh', '#', '#$ -N TEST', '#', '#$ -cwd', 
+                           '#$ -l h_rt=6:00:00', '#', '#$ -j y', '#',
+                           '#$ -P large_mem', '#', #'#$ -pe shm 6', '#', 
+                            '#$ -S /bin/bash', '#', ''),
+                qsubCommandLines = CommandLine, 
+                    scriptName = 'AlignRNAseq')
     
-  }
+#  }
 }
